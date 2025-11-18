35dff216f6cf6c6cad2f2d074ea60cd621602e5c
mehrshad <mehrshadkhakifirooz@gmail.com>
2025-11-18 20:26:48 +0330

    insert to tables

--- OneToOneRelation2/Models/ContactInfo.cs
+++ OneToOneRelation2/Models/ContactInfo.cs
@@ -1,4 +1,5 @@
-﻿using OneToOneRelation2.Models;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using OneToOneRelation2.Models;
 
 namespace OneToOneRelation2;
 
@@ -7,6 +8,8 @@ public class ContactInfo
     public int Id { get; set; }
     public string Mobile { get; set; }
     public string Address { get; set; }
+
+  //  [ForeignKey("Employee")] 
     public int EmployeeId { get; set; }
     public Employee Employee { get; set; }
 }
--- OneToOneRelation2/Program.cs
+++ OneToOneRelation2/Program.cs
@@ -1,8 +1,23 @@
 ﻿
 
+using OneToOneRelation2;
 using OneToOneRelation2.Models;
 
-using (var context = new AppDb())
+using AppDb context = new();
+var contactInfo = new ContactInfo()
 {
-    context.Database.EnsureCreated();
-}
+    Mobile = "123-456-7890",
+    Address = "123 Main St, Anytown, USA"
+};
+var employee = new Employee()
+{
+    Name = "John Doe",
+    famFamilyily = "Doe Family",
+    ContactInfo = contactInfo
+};
+
+context.Employees.Add(employee);
+context.SaveChanges();
+
+
+Console.ReadKey();
